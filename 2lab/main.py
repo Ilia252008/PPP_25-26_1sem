25fca182d6f1d8c0fffac32259814069fbb5fd50
Ilia252008 <iliaardzinba90@gmail.com>
2025-11-12 22:22:28 +0300

    Римский калькулятор

--- 2lab/main.py
+++ 2lab/main.py
@@ -1,4 +1,55 @@
 
 
-if __name__ == "__main__":
-    pass # Ваш код здесь
+def roman_to_int(roman):
+    values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    prev = 0
+    
+    for char in roman[::-1]:
+        current = values[char]
+        if current < prev:
+            result -= current
+        else:
+            result += current
+        prev = current
+    
+    return result
+
+def int_to_roman(num):
+    values = [
+        (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
+        (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'),
+        (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
+    ]
+    
+    result = ''
+    for val, symbol in values:
+        while num >= val:
+            result += symbol
+            num -= val
+    return result
+
+# Ввод данных
+a = input().strip()
+b = input().strip()
+op = input().strip()
+
+# Преобразование
+x = roman_to_int(a)
+y = roman_to_int(b)
+
+# Вычисление
+if op == '+':
+    res = x + y
+elif op == '-':
+    res = x - y
+elif op == '*':
+    res = x * y
+elif op == '/':
+    res = x // y
+
+# Проверка и вывод
+if res < 1:
+    print("Ошибка")
+else:
+    print(int_to_roman(res))
